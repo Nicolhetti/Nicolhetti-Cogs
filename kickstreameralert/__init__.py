2db1faaa0e451ff8af2f118810aefeb97be557b1
Nicolhetti <143234675+Nicolhetti@users.noreply.github.com>
2024-07-10 07:36:23 -0300

    corrected __init__.py

--- kickstreameralert/__init__.py
+++ kickstreameralert/__init__.py
@@ -2,3 +2,7 @@ from .kickstreameralert import KickStreamerAlert
 
 async def setup(bot):
     await bot.add_cog(KickStreamerAlert(bot))
+
+
+# I'm very sorry if something doesn't work well, I'm very new to programming and I use AI to help me :(.
+# Lo siento mucho si algo no funciona bien, soy muy nuevo en programación y uso IA para ayudarme :(.
